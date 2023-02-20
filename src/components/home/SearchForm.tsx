80f12319d77004870a42d2bfe828be10b096f6a6
nelson <nelson@orbitweb.ca>
2023-02-20 00:35:22 -0600

    finished changes in home/SearchForm component and adding zustand to the project

--- src/components/home/SearchForm.tsx
+++ src/components/home/SearchForm.tsx
@@ -1,3 +1,56 @@
+import * as Yup from 'yup';
+import { Title, Image, Box, Stack, TextInput, Button } from '@mantine/core';
+import { useForm, yupResolver } from '@mantine/form';
+
+const schema = Yup.object().shape({
+  username: Yup.string().required(),
+});
+
 export const SearchForm = () => {
-  return <></>;
+  const form = useForm({
+    validate: yupResolver(schema),
+    validateInputOnBlur: true,
+    validateInputOnChange: true,
+    initialValues: {
+      username: '',
+    },
+  });
+
+  return (
+    <Stack align='center'>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          gap: 20,
+        }}>
+        <Title fw={600} order={3}>
+          Github-Profiler
+        </Title>
+        <Image src='/octocat.svg' height={60} width={60} />
+      </Box>
+      <Box w={{ lg: '75%' }}>
+        <form
+          autoComplete='off'
+          onSubmit={form.onSubmit((values) => {
+            // eslint-disable-next-line no-console
+            console.log(values);
+          })}
+          onReset={form.onReset}>
+          <TextInput
+            withAsterisk
+            label='username'
+            type='text'
+            placeholder='search a username'
+            error={form.isDirty()}
+            {...form.getInputProps('username')}
+          />
+          <Button mt={20} type='submit' disabled={!form.isValid()}>
+            Submit
+          </Button>
+        </form>
+      </Box>
+    </Stack>
+  );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,19 @@
+import { Container, Grid } from '@mantine/core';
+import { SearchForm } from '../components/home';
+
 const Home = () => {
   return (
-    <>
-      <p>hola mundo</p>
-    </>
+    <Container fluid>
+      <Grid
+        sx={{
+          margin: '150px auto',
+        }}>
+        <Grid.Col md={6}>
+          <SearchForm />
+        </Grid.Col>
+        <Grid.Col md={6}></Grid.Col>
+      </Grid>
+    </Container>
   );
 };
 export default Home;
